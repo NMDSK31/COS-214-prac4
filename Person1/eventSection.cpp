1db35fa6e25c206d9e576d8eb22f780c4fd8f4e3
Ntando <dlaminintando67@gmail.com>
2026-09-05 22:48:07 +0200

    implementation of composite objects

---
+++ Person1/eventSection.cpp
@@ -0,0 +1,11 @@
+#include "eventSection.h"
+
+EventSection::EventSection(const string& id, const string& name, const string& sectionCode)
+: WorkGroup(id,name),sectionCode(sectionCode)
+{
+
+}
+const string& EventSection::getSectionCode() const
+{
+    return sectionCode;
+}
